e55325e953a3521d3f96dd52a9891d4e90a24cff
Haroon Feisal <38823870+haroonf@users.noreply.github.com>
2019-09-29 12:35:53 -0400

    Update fibonacci.js

--- fibonacci.js
+++ fibonacci.js
@@ -8,6 +8,7 @@ function fib(n) {
   }
   return result;
 }
+
 // Much more effecient fibonacci sequence, O(N) instead of O(2^N)
 // Still requires a ton of memory for massive recursive functions
 function betterFib(n, memo) {
@@ -23,6 +24,7 @@ function betterFib(n, memo) {
   memo[n] = result;
   return result;
 }
+
 // Nonrecursive version, this requires much less memory for high numbers
 // Also O(N)
 function nonrecursiveFib(n) {
